873bfd46b26901389d27f00c53cee4facb08e25e
milkymen <dzahelmelik@mail.ru>
2020-09-12 01:56:46 +0300

    second edit

--- JImageDisplay.java
+++ JImageDisplay.java
@@ -7,15 +7,17 @@ import java.awt.image.BufferedImage;
 public class JImageDisplay extends javax.swing.JComponent {
     //image that we use to display
     private  BufferedImage displImage;
+    private Graphics g;
     
     //initializing display image with the specified parans
     JImageDisplay(int width, int height){
         //creating display image
         displImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-        
+        g = displImage.getGraphics();
         //set size of image
         java.awt.Dimension size = new java.awt.Dimension(width,height);
         setPreferredSize(size);
+        this.repaint();
     }
 
     @Override
@@ -23,15 +25,22 @@ public class JImageDisplay extends javax.swing.JComponent {
         // TODO Auto-generated method stub
         super.paintComponent(g);
         g.drawImage(displImage, 0, 0,displImage.getWidth(), displImage.getHeight(), null);
+        this.repaint();
     }
 
-    public void clearImage (Graphics g){
+    public void clearImage (){
         g.setColor(Color.black);
         g.drawImage(displImage, 0, 0,displImage.getWidth(), displImage.getHeight(), null);
+        //g.fillRect(0, 0, displImage.getWidth(), displImage.getHeight());
+        this.repaint();
     }
     
     public void drawPixel(int x, int y, int rgbColor){
-        Graphics g;
-        g.drawImage(displImage, x, y, 1, 1, null);
+        g.setColor(new Color(rgbColor));
+        //g.drawImage(displImage, x, y, 1, 1,null);
+        //g.fillOval(x, y, 1, 1);
+        //g.fillRect(x, y, 1, 1);
+        g.drawString("o", x, y);
+        this.repaint();
     }
 }
